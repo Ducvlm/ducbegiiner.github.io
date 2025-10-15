65a167f9dcbed8302a4999edb087d1088fc41eba
Vũ Lê Minh Đức <Ducvlm.ptit@gmail.com>
2025-10-15 16:29:59 +0700

    Add files via upload

---
+++ practice27.py
@@ -0,0 +1,12 @@
+try:
+    a=int(input('Hãy nhập vào số nguyên thứ nhất:'))
+    b=int(input('Hãy nhập vào số nguyên thứ hai'))
+    if a>b:
+        print('Số thứ nhất phải nhỏ hơn số thứ hai')
+    else:
+        sum=0
+        for i in range(a,b+1):
+            sum+=i
+        print(f'Tổng của dãy số từ {a} đến {b} là {sum}')
+except:
+    print('Đầu vào không hợp lệ')
